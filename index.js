a87cb838e0f8465c352016746a1423fcdaa9612b
siddharthan21 <siddharthan44@gmail.com>
2023-08-27 22:49:10 +0530

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 $(".hide").click(function (e) { 
     e.preventDefault();
     if($(".header-item").css("display")==="none"){
+        $(".ab").css("animation-name","slo")
     $(".header-item").css("display","flex")
     }
     else{
-    $(".header-item").css("display","none")
+        $(".ab").css("animation-name","hidee")
+        setTimeout(function(){
+            $(".header-item").css("display","none")
+        $(".ab").css("animation-name","none")
+        },700);
     }
 });
